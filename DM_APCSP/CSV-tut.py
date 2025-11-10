55ac04b0221554fa4b39a2895ed81eeafe96af85
Idyllidk <devonmilburn21@gmail.com>
2025-11-10 13:32:11 -0500

    stuff

--- DM_APCSP/CSV-tut.py
+++ DM_APCSP/CSV-tut.py
@@ -4,4 +4,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #Create our dataframe using our csv
-data = pd.read_csv() 
+data = pd.read_csv(r"DM_APCSP\apcspb3.csv")
+
+
+df = pd.DataFrame(data)
+
+print("-_"*40)
+print("Head of the Data Frame:")
+print(df.head())
+
+print("-_"*40)
+print("Tail of the Data Frame:")
+print(df.tail())
+
+print("-_"*40)
+print("Information about the Data Frame:")
+print(df.info())
+
+print("-_"*40)
+print(round(df.describe(),1))
+
+print("-_"*40)
+print("Pie Value Counts")
+print(df['PIE'].value_counts())
+
+print("-_"*40)
+print(df.groupby('PIE')['NAP'].mean())
