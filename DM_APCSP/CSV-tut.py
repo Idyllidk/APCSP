1a8b1917cb037311d44d424e06643231519192c7
Idyllidk <devonmilburn21@gmail.com>
2025-11-10 12:08:47 -0500

    created CSV Tutorial

---
+++ DM_APCSP/CSV-tut.py
@@ -0,0 +1,7 @@
+#Import all necessary libraries - PD, PLT, and NP
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Create our dataframe using our csv
+data = pd.read_csv() 
